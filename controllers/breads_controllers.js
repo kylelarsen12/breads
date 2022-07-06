a05ac1d266092ff3332043bb86b001426e44e1a6
unknown <kylelarsen12@gmail.com>
2022-07-06 18:49:18 -0400

    Add show route and css frameworks

--- controllers/breads_controllers.js
+++ controllers/breads_controllers.js
@@ -10,7 +10,7 @@ breads.get("/", (req, res) => {
 
 //SHOW
 breads.get("/:arrayIndex", (req, res) => {
-  res.send(Bread[req.params.arrayIndex]);
+  res.render("Show", { bread: Bread[req.params.arrayIndex] });
 });
 
 module.exports = breads;
