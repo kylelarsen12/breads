4008b6ebe79d03c1e3c530f745e7a170bb658703
unknown <kylelarsen12@gmail.com>
2022-07-18 20:31:18 -0400

    Snapshot

--- controllers/breads_controllers.js
+++ controllers/breads_controllers.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const breads = express.Router();
 const Bread = require("../models/bread.js");
+const Baker = require("../models/baker.js");
 
 //Index
 breads.get("/", (req, res) => {
@@ -12,7 +13,9 @@ breads.get("/", (req, res) => {
 
 //New
 breads.get("/new", (req, res) => {
-  res.render("new");
+  Baker.find().then((foundBakers) => {
+    res.render("new", { bakers: foundBakers });
+  });
 });
 
 //GET EDIT
